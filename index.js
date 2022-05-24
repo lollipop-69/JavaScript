b8f5d1a79cdf7a111f6d45a13c9d7bd1f9fb03ff
Lollipop69 <94169753+lollipop-69@users.noreply.github.com>
2022-05-24 21:26:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 let varFirst = "hello world";
 let varSecond = 1;
+let varThird = 2;
+let varFourth = "goodbye world";
 
-if(varSecond === 1){
+if(varSecond === varThird){
     console.log(varFirst)
 }
 else{
-    console.log("Failed")
+    console.log(varFourth)
 }
+
+function hello(args){
+    console.log(args)
+}
+
+hello("hello")
